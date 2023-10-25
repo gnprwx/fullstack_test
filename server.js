c1fde40dccc8dd67cbf881058166d2bc27227ff2
Greg Pappas <gnpappas@proton.me>
2023-10-25 10:56:52 -1000

    Add route for innerjoin to show members belonging to what band

--- server.js
+++ server.js
@@ -18,15 +18,27 @@ await client.connect();
 app.get("/fst/musicians", (req, res) => {
     console.log("test!");
     client.query(`SELECT * FROM musicians`).then((data) => {
-        res.send(data);
+        res.send(data.rows);
     });
 });
 
 app.get("/fst/band", (req, res) => {
     console.log("test!");
     client.query(`SELECT * FROM band`).then((data) => {
-        res.send(data);
+        res.send(data.rows);
     });
+});
+
+app.get("/fst/band/:id", (req, res) => {
+    const id = req.params.id;
+    client
+        .query(
+            `SELECT musicians.member1, musicians.member2, musicians.member3, musicians.member4, band.name, band.genre FROM musicians INNER JOIN band ON musicians.band_id = band.id WHERE band.id = $1`,
+            [id]
+        )
+        .then((data) => {
+            res.send(data.rows[0]);
+        });
 });
 
 app.listen(PORT, () => {
