332f0fd23760adb36c693e4fb6f9001ee6809943
Greg Pappas <gnpappas@proton.me>
2023-10-25 09:06:24 -1000

    Add routes w/ one-to-many relationship

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
-const {PORT, DATABASE_URL} = process.env;
+const { PORT, DATABASE_URL } = process.env;
 const client = new pg.Client({
     connectionString: DATABASE_URL,
 });
@@ -15,10 +15,19 @@ app.use(express.json());
 
 await client.connect();
 
-app.get('/fst', (req, res) => {
-    console.log('test!');
-    res.send('x');
-})
+app.get("/fst/musicians", (req, res) => {
+    console.log("test!");
+    client.query(`SELECT * FROM musicians`).then((data) => {
+        res.send(data);
+    });
+});
+
+app.get("/fst/band", (req, res) => {
+    console.log("test!");
+    client.query(`SELECT * FROM band`).then((data) => {
+        res.send(data);
+    });
+});
 
 app.listen(PORT, () => {
     console.log(`Connected on port ${PORT}`);
